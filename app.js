ff018343091149ce54b98328de1b44c52656cacd
Jesús Antonio Zúñiga Arce <jzuniga@uabcs.mx>
2019-10-22 23:05:11 -0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // const API = 'http://api.tvmaze.com/search/shows?q='
 
-// const btnLogin = document.querySelector('#btnLogin')
-// btnLogin.addEventListener('click', function() {
-// 	window.location.href = 'home.html'
-// })
+
+const loadLogin = function () {
+	const btnLogin = document.querySelector('#btnLogin')
+	btnLogin.addEventListener('click', function() {
+		window.location.href = 'home.html'
+	})
+}
